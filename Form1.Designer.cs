ddfa0cf57afe51693b4470ffa0c8e8c5fa2ce881
setsumi <setsumi@users.noreply.github.com>
2022-11-17 21:16:04 +1000

    initial code

---
+++ Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace USBeject
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.tbInfo = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.lblProcessName = new System.Windows.Forms.Label();
+            this.btnKill = new System.Windows.Forms.Button();
+            this.tbPID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbInfo
+            // 
+            this.tbInfo.BackColor = System.Drawing.SystemColors.Window;
+            this.tbInfo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbInfo.Location = new System.Drawing.Point(0, 0);
+            this.tbInfo.Multiline = true;
+            this.tbInfo.Name = "tbInfo";
+            this.tbInfo.ReadOnly = true;
+            this.tbInfo.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbInfo.Size = new System.Drawing.Size(671, 383);
+            this.tbInfo.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnRefresh);
+            this.panel1.Controls.Add(this.lblProcessName);
+            this.panel1.Controls.Add(this.btnKill);
+            this.panel1.Controls.Add(this.tbPID);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 383);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(671, 31);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRefresh.Location = new System.Drawing.Point(584, 4);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 3;
+            this.btnRefresh.Text = "Refresh (F5)";
+            this.toolTip1.SetToolTip(this.btnRefresh, "Refresh event list (F5)");
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // lblProcessName
+            // 
+            this.lblProcessName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblProcessName.AutoSize = true;
+            this.lblProcessName.Location = new System.Drawing.Point(261, 9);
+            this.lblProcessName.Name = "lblProcessName";
+            this.lblProcessName.Size = new System.Drawing.Size(83, 13);
+            this.lblProcessName.TabIndex = 4;
+            this.lblProcessName.Text = "lblProcessName";
+            // 
+            // btnKill
+            // 
+            this.btnKill.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnKill.Location = new System.Drawing.Point(171, 4);
+            this.btnKill.Name = "btnKill";
+            this.btnKill.Size = new System.Drawing.Size(75, 23);
+            this.btnKill.TabIndex = 2;
+            this.btnKill.Text = "Close/Kill";
+            this.toolTip1.SetToolTip(this.btnKill, "Close offending process");
+            this.btnKill.UseVisualStyleBackColor = true;
+            this.btnKill.Click += new System.EventHandler(this.btnKill_Click);
+            // 
+            // tbPID
+            // 
+            this.tbPID.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.tbPID.Location = new System.Drawing.Point(82, 6);
+            this.tbPID.Name = "tbPID";
+            this.tbPID.Size = new System.Drawing.Size(73, 20);
+            this.tbPID.TabIndex = 0;
+            this.tbPID.TextChanged += new System.EventHandler(this.tbPID_TextChanged);
+            this.tbPID.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbPID_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Process ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Loading...";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(671, 414);
+            this.Controls.Add(this.tbInfo);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "FormMain";
+            this.Text = "USBeject";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.Shown += new System.EventHandler(this.FormMain_Shown);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FormMain_KeyDown);
+            this.Resize += new System.EventHandler(this.FormMain_Resize);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbInfo;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblProcessName;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnKill;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbPID;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,414 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+
+namespace USBeject
+{
+    public partial class FormMain : Form
+    {
+        //==============================================================
+        private const uint WM_MOUSEMOVE = 0x0200;
+        private const uint WM_CLOSE = 0x0010;
+        [return: MarshalAs(UnmanagedType.Bool)]
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        static extern IntPtr PostMessage(IntPtr hWnd, uint Msg, int wParam, int lParam);
+        //[DllImport("user32.dll")]
+        //static extern int SendMessage(IntPtr hWnd, uint Msg, int wParam, int lParam);
+
+        //-----------------
+        private enum GWL : int
+        {
+            GWL_WNDPROC = (-4),
+            GWL_HINSTANCE = (-6),
+            GWL_HWNDPARENT = (-8),
+            GWL_STYLE = (-16),
+            GWL_EXSTYLE = (-20),
+            GWL_USERDATA = (-21),
+            GWL_ID = (-12)
+        }
+        private enum WindowStyles : uint
+        {
+            WS_POPUP = 0x80000000,
+            WS_CHILD = 0x40000000,
+            WS_OVERLAPPED = 0x0
+        }
+        [DllImport("user32.dll")]
+        static extern IntPtr GetWindowLong(IntPtr hWnd, int nIndex);
+
+        //-----------------
+        delegate bool EnumThreadDelegate(IntPtr hWnd, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        static extern bool EnumThreadWindows(int dwThreadId, EnumThreadDelegate lpfn, IntPtr lParam);
+        static IEnumerable<IntPtr> EnumerateProcessWindowHandles(int processId)
+        {
+            var handles = new List<IntPtr>();
+
+            foreach (ProcessThread thread in Process.GetProcessById(processId).Threads)
+                EnumThreadWindows(thread.Id,
+                        (hWnd, lParam) => { handles.Add(hWnd); return true; }, IntPtr.Zero);
+
+            return handles;
+        }
+        //==============================================================
+
+        string _processName = "";
+        bool _initForm = true;
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        void AppendText(XmlNode n, string s, bool newline = true)
+        {
+            bool isnode = n != null;
+            if (!isnode) newline = false;
+            tbInfo.AppendText((isnode ? n.LocalName + ": " : "") + (newline ? Environment.NewLine : "") + s + Environment.NewLine);
+        }
+
+        private void FormMain_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Escape:
+                    Close();
+                    break;
+                case Keys.F5:
+                    btnRefresh_Click(null, null);
+                    break;
+            }
+        }
+
+        class USBDeviceInfo
+        {
+            public USBDeviceInfo(string deviceId, string name, string description)
+            {
+                DeviceId = deviceId;
+                Name = name;
+                Description = description;
+            }
+
+            public string DeviceId { get; }
+            public string Name { get; }
+            public string Description { get; }
+
+            public override string ToString()
+            {
+                return Name;
+            }
+        }
+
+        static USBDeviceInfo GetUSBDevice(string deviceID)
+        {
+            USBDeviceInfo rv = null;
+            using (var mos = new ManagementObjectSearcher(@"Select * From Win32_PnPEntity"))
+            {
+                using (ManagementObjectCollection collection = mos.Get())
+                {
+                    foreach (var device in collection)
+                    {
+                        var id = device.GetPropertyValue("DeviceId").ToString();
+
+                        if (!id.StartsWith("USB", StringComparison.OrdinalIgnoreCase))
+                            continue;
+
+                        var name = device.GetPropertyValue("Name").ToString();
+                        var description = device.GetPropertyValue("Description").ToString();
+                        if (id == deviceID)
+                        {
+                            rv = new USBDeviceInfo(id, name, description);
+                            break;
+                        }
+                    }
+                }
+            }
+            return rv;
+        }
+
+        private void FormMain_Shown(object sender, EventArgs e)
+        {
+            if (Properties.Settings.Default.winWidth > 0)
+                this.Width = Properties.Settings.Default.winWidth;
+            if (Properties.Settings.Default.winHeight > 0)
+                this.Height = Properties.Settings.Default.winHeight;
+            if (Properties.Settings.Default.winMaximized)
+                this.WindowState = FormWindowState.Maximized;
+
+            ScrollToBottom();
+
+            _initForm = false;
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            GetEvents();
+            timer1.Start();
+        }
+
+        void ScrollToBottom()
+        {
+            if (tbInfo.Visible)
+            {
+                tbInfo.SelectionStart = tbInfo.TextLength;
+                tbInfo.ScrollToCaret();
+            }
+        }
+
+        void GetEvents()
+        {
+            string eventID = "225";
+            string LogSource = "System";
+            string sQuery = "*[System/EventID=" + eventID + "]";
+
+            string PID = "";
+
+            var elQuery = new EventLogQuery(LogSource, PathType.LogName, sQuery);
+            using (var elReader = new EventLogReader(elQuery))
+            {
+                elReader.Seek(System.IO.SeekOrigin.End, -5);
+                EventRecord eventInstance = elReader.ReadEvent();
+                try
+                {
+                    for (; null != eventInstance; eventInstance = elReader.ReadEvent())
+                    {
+                        //Access event properties here:
+                        //eventInstance.LogName;
+                        //eventInstance.ProviderName;
+
+                        XmlDocument doc = new XmlDocument();
+                        doc.LoadXml(eventInstance.ToXml());
+                        XmlNode node = doc.DocumentElement.SelectSingleNode("/*[local-name()='Event']/*[local-name()='System']");
+
+                        AppendText(null, "------------- ");
+                        int offset = 1;
+                        foreach (XmlNode n in node.ChildNodes)
+                        {
+                            switch (n.LocalName)
+                            {
+                                case "Provider":
+                                    offset++;
+                                    AppendText(n, n.Attributes["Name"].Value, false);
+                                    break;
+                                case "EventID":
+                                    offset++;
+                                    AppendText(n, n.InnerText, false);
+                                    break;
+                                case "TimeCreated":
+                                    offset++;
+                                    //AppendText(n, DateTime.Parse(n.Attributes["SystemTime"].Value).ToLocalTime().ToString(), false);
+                                    string[] tempArray = tbInfo.Lines;
+                                    tempArray[tempArray.Count() - offset] += DateTime.Parse(n.Attributes["SystemTime"].Value).ToLocalTime().ToString() + " -------------";
+                                    tbInfo.Lines = tempArray;
+                                    break;
+                            }
+                        }
+
+                        node = node.NextSibling;
+                        foreach (XmlNode n in node.ChildNodes)
+                        {
+                            if (!n.Attributes["Name"].Value.Contains("Length"))
+                            {
+                                AppendText(null, n.Attributes["Name"].Value + ": " + n.InnerText);
+
+                                if (n.Attributes["Name"].Value == "DeviceInstance")
+                                {
+                                    USBDeviceInfo di = GetUSBDevice(n.InnerText);
+                                    if (di != null)
+                                    {
+                                        AppendText(null, "DeviceName: " + di.Name
+                                            + " (" + di.Description + ")");
+                                    }
+                                    else
+                                    {
+                                        AppendText(null, "DEVICE NOT FOUND");
+                                    }
+                                }
+                                else if (n.Attributes["Name"].Value == "ProcessId")
+                                {
+                                    PID = n.InnerText;
+                                }
+                                else if (n.Attributes["Name"].Value == "ProcessName")
+                                {
+                                    _processName = n.InnerText;
+                                }
+                            }
+                        }
+                    }
+                }
+                finally
+                {
+                    if (eventInstance != null)
+                        eventInstance.Dispose();
+                }
+            }
+
+            tbPID.Text = PID;
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            tbInfo.Visible = false;
+            tbInfo.Clear();
+            GetEvents();
+            tbInfo.Visible = true;
+            ScrollToBottom();
+            tbInfo.Focus();
+            DetectProcess();
+        }
+
+        private void btnKill_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(tbPID.Text))
+            {
+                int pid = 0;
+                try { pid = int.Parse(tbPID.Text); } catch { }
+                if (pid > 0)
+                    KillProcess(pid);
+            }
+        }
+
+        void KillProcess(int pid)
+        {
+            Process p = null;
+            try { p = Process.GetProcessById(pid); } catch { }
+            if (p != null)
+            {
+                IntPtr hmain = IntPtr.Zero;
+                if (p.MainWindowHandle != IntPtr.Zero)
+                {
+                    hmain = p.MainWindowHandle;
+                    try { p.CloseMainWindow(); }
+                    catch { }
+                }
+                if (hmain == IntPtr.Zero || !p.WaitForExit(1000))
+                {
+                    foreach (var handle in EnumerateProcessWindowHandles(p.Id))
+                    {
+                        IntPtr style = GetWindowLong(handle, (int)GWL.GWL_STYLE);
+                        if (((UInt64)style & (UInt64)WindowStyles.WS_POPUP) != 0)
+                        {
+                        }
+                        else if (((UInt64)style & (UInt64)WindowStyles.WS_CHILD) != 0)
+                        {
+                        }
+                        else // WS_OVERLAPPED
+                        {
+                            // close all overlapped windows
+                            if (handle != hmain)
+                                PostMessage(handle, WM_CLOSE, 0, 0);
+                        }
+                    }
+                    if (!p.WaitForExit(1000))
+                    {
+                        p.Kill();
+                    }
+                }
+                p.Close();
+                p.Dispose();
+                //Thread.Sleep(50);
+            }
+        }
+
+        private void tbPID_TextChanged(object sender, EventArgs e)
+        {
+            DetectProcess();
+        }
+
+        void DetectProcess()
+        {
+            int pid = 0;
+            try { pid = int.Parse(tbPID.Text); } catch { }
+            if (pid == 0)
+            {
+                lblProcessName.Text = "";
+            }
+            else
+            {
+                string name = GetProcessName(pid);
+                lblProcessName.Text = name == null ? "Process not found" : name;
+                if (Path.GetFileName(name) == Path.GetFileName(_processName))
+                {
+                    toolTip1.SetToolTip(lblProcessName, lblProcessName.Text);
+                    lblProcessName.ForeColor = SystemColors.ControlText;
+                }
+                else
+                {
+                    toolTip1.SetToolTip(lblProcessName, "Warning: Process is different!" + Environment.NewLine + lblProcessName.Text);
+                    lblProcessName.ForeColor = Color.Red;
+                }
+            }
+        }
+
+        string GetProcessName(int pid)
+        {
+            string rv = null;
+            Process proc = null;
+            try { proc = Process.GetProcessById(pid); } catch { }
+            if (proc != null)
+            {
+                try { rv = proc.MainModule.FileName; } catch { }
+                proc.Close();
+                proc.Dispose();
+            }
+            return rv;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DetectProcess();
+        }
+
+        private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.Save();
+        }
+
+        FormWindowState _lastWindowState = FormWindowState.Normal;
+        private void FormMain_Resize(object sender, EventArgs e)
+        {
+            if (_initForm) return;
+
+            if (WindowState != _lastWindowState)
+            {
+                _lastWindowState = WindowState;
+                if (WindowState == FormWindowState.Maximized) // Maximized!
+                {
+                    Properties.Settings.Default.winMaximized = true;
+                }
+                else if (WindowState == FormWindowState.Normal)
+                {
+                    Properties.Settings.Default.winMaximized = false;
+                }
+            }
+            else if (WindowState == FormWindowState.Normal) // resize
+            {
+                Properties.Settings.Default.winWidth = this.Width;
+                Properties.Settings.Default.winHeight = this.Height;
+            }
+        }
+
+        private void tbPID_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Enter:
+                    btnKill_Click(null, null);
+                    break;
+            }
+        }
+    }
+}
